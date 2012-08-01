f99ea5e5a543ec574fe10a7e7cd8aa802d0a8ea6
Jordan Scales <scalesjordan@gmail.com>
2012-08-01 18:19:00 -0400

    introducing new bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,38 @@
 $(function() {
-  $('.order input[type="checkbox"]').on('click', function(e) {
+
+  $('#highlight_d').click(function() {
+    $('.table:eq(4)').addClass('highlighted');
+  });
+
+  $('#highlight_all').click(function() {
+    
+  });
+
+  $('#remove_last').click(function() {
+    $('.table :last').remove();
+  });
+
+  $('#change_a').click(function() {
+    $(this).addClass('blue');
+  });
+
+  $('#change_c').click(function() {
+    $(this).addClass('.green');
+  });
+
+  $('.order input[type="checkbox"]').live('click', function(e) {
     if ($(this).attr('checked')) {
       checkoff(this);
     } else {
       uncheck(this);
     }
-  });  
+  });
+
+  $('#new_order').on('submit', function(e) {
+    e.preventDefault();
+    $('#orders').append('<li class="order">' + $('#order_name').val() + '</li>');
+    $('#order_name').val('');
+  });
 });
 
 var checkoff = function(el) {
